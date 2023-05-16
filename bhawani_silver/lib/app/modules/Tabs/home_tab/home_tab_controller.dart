a6c477e7ddbcadfcd1017e5333d167eb19e98ccd
Rajkumarlohar65 <78910492+Rajkumarlohar65@users.noreply.github.com>
2023-05-16 16:27:26 +0530

    removing extra semicolon

--- bhawani_silver/lib/app/modules/Tabs/home_tab/home_tab_controller.dart
+++ bhawani_silver/lib/app/modules/Tabs/home_tab/home_tab_controller.dart
@@ -1,4 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';;
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 
 class HomeTabController extends GetxController {

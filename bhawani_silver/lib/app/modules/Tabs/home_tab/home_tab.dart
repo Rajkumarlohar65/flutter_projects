752475c7eb39afa86f26629b7e5615683f5bd2a5
Rajkumarlohar65 <78910492+Rajkumarlohar65@users.noreply.github.com>
2023-05-16 14:14:37 +0530

    updating ui of home tab

--- bhawani_silver/lib/app/modules/Tabs/home_tab/home_tab.dart
+++ bhawani_silver/lib/app/modules/Tabs/home_tab/home_tab.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:BhawaniSilver/app/modules/Tabs/home_tab/home_tab_controller.dart';
 import 'package:BhawaniSilver/app/routes/app_pages.dart';
 import 'package:BhawaniSilver/app/widgets/dialog_box_widget/image_dialog_widget.dart';
@@ -10,6 +11,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 
 import '../../../core/values/app_color.dart';
 import '../../../data/model/product.dart';
+import '../../home/my_search_delegate.dart';
 
 class HomeTab extends GetView<HomeTabController> {
   const HomeTab({super.key});
@@ -32,74 +34,126 @@ class HomeTab extends GetView<HomeTabController> {
             final productDocs = productSnapShot.data!.docs;
             final products =
                 productDocs.map((doc) => Product.fromSnapshot(doc)).toList();
-            return ListView.separated(
-              itemCount: productDocs.length,
-              itemBuilder: (context, index) {
-                final product = products[index];
+            int currentIndex = 0;
 
-                return GestureDetector(
-                  onTap: () {
-                    Get.toNamed(Routes.OVERVIEW_OF_PRODUCT, arguments: product);
-                  },
-                  child: SizedBox(
-                    height: 150,
-                    child: ListTile(
-                      title: Row(
-                        children: [
-                          GestureDetector(
-                            onTap: () {
-                              showDialog(
-                                  context: context,
-                                  builder: (context) => ImageDialogWidget(
-                                        image_url: product.image,
-                                      ));
-                            },
-                            child: AspectRatio(
-                                aspectRatio: 1,
-                                child: CachedNetworkImage(
-                                  imageUrl: product.image,
-                                  cacheManager: DefaultCacheManager(),
-                                  placeholder: (context, imageUrl) {
-                                    return const Center(
-                                      child: SpinKitFadingCircle(
-                                        size: 20,
-                                        color: AppColor.blueColor,
-                                      ),
-                                    );
-                                  },
-                                )),
-                          ),
-                          const SizedBox(
-                            width: 16,
-                          ),
-                          Expanded(
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  product.name,
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .titleLarge?.copyWith(color: AppColor.blackColor),
-                                  maxLines: 2,
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                                const SizedBox(height: 8),
-                                Text('Price: \$${product.price}'),
-                              ],
-                            ),
-                          )
-                        ],
+            Timer.periodic(const Duration(seconds: 10), (timer) {
+              currentIndex = (currentIndex + 1) % products.length;
+              controller.updateCurrentIndex(
+                  currentIndex); // Update the index in the controller
+            });
+            return CustomScrollView(
+              slivers: [
+                SliverAppBar(
+                  title: Row(
+                    children: [
+                      const Expanded(child: Text("Bhawani Silver")),
+                      InkWell(
+                        child: const Icon(Icons.search),
+                        onTap: () {
+                          showSearch(
+                              context: context, delegate: MySearchDelegate());
+                        },
                       ),
+                    ],
+                  ),
+                  expandedHeight: 200,
+                  pinned: true,
+                  flexibleSpace: FlexibleSpaceBar(
+                    background: Padding(
+                      padding: const EdgeInsets.only(bottom: 5),
+                      child: Obx(() => InkWell(
+                            onTap: () {
+                              final product =
+                                  products[controller.currentIndex.value];
+                              Get.toNamed(Routes.OVERVIEW_OF_PRODUCT,
+                                  arguments: product);
+                            },
+                            child: CachedNetworkImage(
+                              imageUrl:
+                                  products[controller.currentIndex.value].image,
+                            ),
+                          )),
                     ),
                   ),
-                );
-              },
-              separatorBuilder: (BuildContext context, int index) =>
-                  const SizedBox(
-                height: 7,
-              ),
+                ),
+                SliverPadding(
+                  padding: const EdgeInsets.only(),
+                  sliver: SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                      (context, index) {
+                        final product = products[index];
+                        return GestureDetector(
+                          onTap: () {
+                            Get.toNamed(Routes.OVERVIEW_OF_PRODUCT,
+                                arguments: product);
+                          },
+                          child: SizedBox(
+                            height: 150,
+                            child: ListTile(
+                              title: Row(
+                                children: [
+                                  GestureDetector(
+                                    onTap: () {
+                                      showDialog(
+                                        context: context,
+                                        builder: (context) => ImageDialogWidget(
+                                          image_url: product.image,
+                                        ),
+                                      );
+                                    },
+                                    child: AspectRatio(
+                                      aspectRatio: 1,
+                                      child: CachedNetworkImage(
+                                        imageUrl: product.image,
+                                        cacheManager: DefaultCacheManager(),
+                                        placeholder: (context, imageUrl) {
+                                          return const Center(
+                                            child: SpinKitFadingCircle(
+                                              size: 20,
+                                              color: AppColor.blueColor,
+                                            ),
+                                          );
+                                        },
+                                      ),
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    width: 16,
+                                  ),
+                                  Expanded(
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          product.name,
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .titleLarge
+                                              ?.copyWith(
+                                                color: AppColor.blackColor,
+                                              ),
+                                          maxLines: 2,
+                                          overflow: TextOverflow.ellipsis,
+                                        ),
+                                        const SizedBox(height: 8),
+                                        Text('Price: \$${product.price}'),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                      childCount: products.length,
+                    ),
+                  ),
+                ),
+              ],
             );
           }
         },
--- bhawani_silver/lib/app/modules/Tabs/home_tab/home_tab_controller.dart
+++ bhawani_silver/lib/app/modules/Tabs/home_tab/home_tab_controller.dart
@@ -1,17 +1,24 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/animation.dart';
 import 'package:get/get.dart';
 
 class HomeTabController extends GetxController {
   late final Stream<QuerySnapshot> productStream;
+  RxInt currentIndex = 0.obs;
 
   @override
   Future<void> onInit() async {
-    super.onInit();
     await _initializeProductStream();
+
+    super.onInit();
   }
 
   Future<void> _initializeProductStream() async {
     productStream = FirebaseFirestore.instance.collection('products').snapshots();
+  }
+
+  void updateCurrentIndex(int index) {
+    currentIndex.value = index;
   }
 
 }
